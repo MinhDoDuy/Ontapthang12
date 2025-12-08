6575f6261e8793aaa11b6c83456fe1f25b3ca345
gitbasic <doduyminh178@gmail.com>
2025-12-08 14:12:17 +0700

    commit ngay 8/12

--- ngay_8_12/Operators.py
+++ ngay_8_12/Operators.py
@@ -17,3 +17,24 @@ print("so ky tu: ", len(n))
 print("-----------------------------")
 
 #3. Đếm số từ
+text = input("Nhap chuoi: ")
+word = text.split()
+so_tu = len(word)
+
+print("so tu trong cau la: ", so_tu)
+print("----------------------------")
+
+#4. Chương trình xử lý chuỗi
+text1 = input("Nhập 1 câu: ").strip().lower()
+
+words = text1.split()
+so_tu = len(words)
+
+so_chu_cai = 0
+for ch in text1:
+    if ch.isalpha():
+        so_chu_cai += 1
+
+print("Số từ:", so_tu)
+print("Số chữ cái:", so_chu_cai)
+
