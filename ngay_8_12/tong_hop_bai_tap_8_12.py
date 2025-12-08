8929e3cdc1cdeb4a5e6367f76a48ec536f2973c3
gitbasic <doduyminh178@gmail.com>
2025-12-08 10:51:42 +0700

    commit ngay 8/12

---
+++ ngay_8_12/tong_hop_bai_tap_8_12.py
@@ -0,0 +1,19 @@
+#1. Máy tính 2 số
+a = float(input("Nhap so a: "))
+b = float(input("Nhap so b: "))
+
+print("Addition a + b: ", a+b)
+print("Subtraction	 a - b: ", a-b)
+print("Multiplication a * b: ", a*b)
+print("Division	a / b: ", a/b)
+print("Modulus a % b: ", a%b)
+print("Exponentiation a ** b: ", a**b)
+print("Floor division a // b: ", a//b)
+print("-----------------------------")
+
+#2. Đếm ký tự trong chuỗi
+n = input("Nhap chuoi: ")
+print("so ky tu: ", len(n))
+print("-----------------------------")
+
+#3. Đếm số từ
