53ac634873232e5adbd8dc081626d6d3695127eb
gitbasic <doduyminh178@gmail.com>
2025-12-08 16:13:18 +0700

    commit ngay 8/12

--- ngay_8_12/Operators.py
+++ ngay_8_12/Operators.py
@@ -2,11 +2,11 @@
 a = float(input("Nhap so a: "))
 b = float(input("Nhap so b: "))
 
-print("Addition a + b: ", a+b)
-print("Subtraction	 a - b: ", a-b)
+print("Addition       a + b: ", a+b)
+print("Subtraction	   a - b: ", a-b)
 print("Multiplication a * b: ", a*b)
-print("Division	a / b: ", a/b)
-print("Modulus a % b: ", a%b)
+print("Division	   a / b: ", a/b)
+print("Modulus        a % b: ", a%b)
 print("Exponentiation a ** b: ", a**b)
 print("Floor division a // b: ", a//b)
 print("-----------------------------")
@@ -37,4 +37,6 @@ for ch in text1:
 
 print("Số từ:", so_tu)
 print("Số chữ cái:", so_chu_cai)
+print("-----------------------------")
+
 
